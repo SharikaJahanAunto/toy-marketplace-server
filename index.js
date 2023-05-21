6bec4af0b4da05854f4fbad76471532ac1c96d79
SharikaJahanAunto <Sharikajahannn@gmail.com>
2023-05-21 17:44:56 +0600

    added single data

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors')
 require('dotenv').config()
+const { ObjectId } = require('mongodb');
+
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -25,12 +27,115 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    
+    const toyCollection = client.db('marketPlaceDB').collection('toys')
+
+    app.get('/toys', async(req, res) =>{
+        const result = await  toyCollection.find().toArray()
+        res.send(result);
+        // res.send(result)
+    })
+
+
+    
+
+
+    app.get('/toys/:id', async (req, res) => {
+        const { id } = req.params;
+        try {
+          const toy = await toyCollection.findOne({ _id: new ObjectId(id) });
+          if (!toy) {
+            return res.status(404).json({ error: 'Toy not found' });
+          }
+          res.json(toy);
+        } catch (error) {
+          console.error('Error fetching toy details:', error);
+          res.status(500).json({ error: 'Internal server error' });
+        }
+      });
+      
+      
+      // Create a new toy
+      // app.post('/toys', async (req, res) => {
+      //   const { name, sellerName, sellerEmail, price, rating, quantity, description } = req.body;
+      //   try {
+      //     const newToy = await toyCollection.insertOne({
+      //       name,
+      //       sellerName,
+      //       sellerEmail,
+      //       price,
+      //       rating,
+      //       quantity,
+      //       description,
+      //     });
+      //     res.status(201).json(newToy.ops[0]);
+      //   } catch (error) {
+      //     console.error('Error creating toy:', error);
+      //     res.status(500).json({ error: 'Internal server error' });
+      //   }
+      // });
+
+      app.post("/toys", async (req, res) => {
+        const toys = req.body;
+        const result = await toyCollection.insertOne(toys);
+        res.json(result);
+      });
+    
+
+      
+      
+      // Update toy details
+      // app.put('/toys/:id', async (req, res) => {
+      //   const { id } = req.params;
+      //   const { toyName, sellerName, sellerEmail, price, rating, quantity, description } = req.body;
+      //   try {
+      //     const updatedToy = await toyCollection.findOneAndUpdate(
+      //       { _id: new ObjectId(id) },
+      //       {
+      //         $set: {
+      //           toyName,
+      //           sellerName,
+      //           sellerEmail,
+      //           price,
+      //           rating,
+      //           quantity,
+      //           description,
+      //         },
+      //       },
+      //       { returnOriginal: false }
+      //     );
+      //     if (!updatedToy.value) {
+      //       return res.status(404).json({ error: 'Toy not found' });
+      //     }
+      //     res.json(updatedToy.value);
+      //   } catch (error) {
+      //     console.error('Error updating toy:', error);
+      //     res.status(500).json({ error: 'Internal server error' });
+      //   }
+      // });
+      
+      // Delete a toy
+      app.delete('/toys/:id', async (req, res) => {
+        const { id } = req.params;
+        try {
+          const deletedToy = await toyCollection.findOneAndDelete({ _id: ObjectId(id) });
+          if (!deletedToy.value) {
+            return res.status(404).json({ error: 'Toy not found' });
+          }
+          res.json({ message: 'Toy deleted successfully' });
+        } catch (error) {
+          console.error('Error deleting toy:', error);
+          res.status(500).json({ error: 'Internal server error' });
+        }
+      });
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
